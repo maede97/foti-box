66aa9a1abf2f2c8bd1af65ceabb472b24aff5175
maede97 <maede97@hotmail.com>
2025-12-13 14:04:41 +0100

    ignore typing on adminschema hook

--- src/models/admin.ts
+++ src/models/admin.ts
@@ -14,9 +14,12 @@ const AdminSchema = new Schema<IAdmin>({
 
 // Hash password before saving
 AdminSchema.pre<IAdmin>('save', async function (next) {
+  // ignore typing error here
+  // @ts-ignore 
   if (!this.isModified('password')) return next();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
+  // @ts-ignore
   next();
 });
 
